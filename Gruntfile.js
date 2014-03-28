5db0b7ac5ef396bcb5e7710e5ad678ff701027f0
Danijel Petrovic <webmaster@ivebe.com>
2014-03-29 00:21:24 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,14 @@
-grunt.registerTask('travis', 'jshint');
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      myFiles: ['cyrlatconverter-v0.6.0.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', ['jshint']);
+
+};
