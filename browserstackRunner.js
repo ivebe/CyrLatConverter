4b0b10b94aba3a1b511a8b12e6c1eff4a3012d7f
ivebe <dan@ivebe.com>
2017-12-27 13:39:13 +0100

    put wrapper back

---
+++ browserstackRunner.js
@@ -0,0 +1,12 @@
+var browserstackRunner = require('browserstack-runner');
+var config = require('./browserstack.json');
+
+browserstackRunner.run(config, function(error, report) {
+    for (var i = 0; i < report.length; i++) {
+      var err = report[i].suites.status;
+      if (err === 'failed') {
+        process.exitCode = 1;
+        return
+      }
+    }
+});
